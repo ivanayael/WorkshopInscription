4a668180a8b9aab57e18ca2c9969133e4a887577
Ivana Yael Curra <icurra@quadiontech.com>
2020-07-16 18:12:21 -0300

    Create WorkshopRepo Code

---
+++ projectcode/WorkshopInscriptions/WorkshopInscriptions/Data/InscriptionItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkshopInscriptions.Data
+{
+    public class InscriptionItem
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public DateTime InscriptionDate { get; set; }
+        public bool HasAttended { get; set; }
+    }
+}
